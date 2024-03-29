d183dda434a0a09e1b865473706d0d420d96022f
Isarok <isaac.lucerito@gmail.com>
2024-03-29 15:31:28 +0100

    add component card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,13 @@
 import Layaout from "../../Components/Layaout";
+import Card from "../../Components/Card";
+
 const Home = () => {
-  return <Layaout>Home</Layaout>;
+  return (
+    <Layaout>
+      Home
+      <Card />
+    </Layaout>
+  );
 };
 
 export default Home;
