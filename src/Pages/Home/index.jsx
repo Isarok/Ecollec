87a4ef4007079a1e7ed77e65e362bdc1e60c29db
Isarok <isaac.lucerito@gmail.com>
2024-03-29 20:50:49 +0100

    add apifake

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from "react";
 import Layaout from "../../Components/Layaout";
 import Card from "../../Components/Card";
 
 const Home = () => {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products").then((response) =>
+      response.json().then((data) => setItems(data))
+    );
+  }, []);
+
   return (
     <Layaout>
       Home
-      <Card />
+      <div className="grid grid-cols-4 w-full max-w-screen-lg gap-4">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </div>
     </Layaout>
   );
 };
