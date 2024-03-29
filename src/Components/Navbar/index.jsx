9da265e168c16e58fec385a41f353c14653c7caf
Isarok <isaac.lucerito@gmail.com>
2024-03-29 21:53:36 +0100

    add first funtionality with context

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ShoppingCartContext } from "../../Context";
 
 const Navbar = () => {
+  const context = useContext(ShoppingCartContext);
   const activeStyle = "underline underline-offset-4";
   return (
     <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-orange-600">
@@ -83,7 +86,7 @@ const Navbar = () => {
             Sign in
           </NavLink>
         </li>
-        <li>🛒7</li>
+        <li>🛒{context.count}</li>
       </ul>
     </nav>
   );
