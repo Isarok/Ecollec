ca120d8f04b8dbf095449acb5863e8e430b199ed
Isarok <isaac.lucerito@gmail.com>
2024-03-30 20:24:57 +0100

    Checkout cart products

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,6 +1,26 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
 import Layaout from "../../Components/Layaout";
+import OrderCard from "../../Components/OrderCard";
 const MyOrder = () => {
-  return <Layaout>MyOrder</Layaout>;
+  const context = useContext(ShoppingCartContext);
+
+  return (
+    <Layaout>
+      MyOrder
+      <div className="flex flex-col w-80">
+        {context.order?.slice(-1)[0].products.map((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+          />
+        ))}
+      </div>
+    </Layaout>
+  );
 };
 
 export default MyOrder;
