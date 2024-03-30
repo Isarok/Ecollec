2ab47fc45dc8a54a684689267e32ed16ce8d3b06
Isarok <isaac.lucerito@gmail.com>
2024-03-30 18:42:37 +0100

    add elements to cart and visualize

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../Context";
+import OrderCard from "../../Components/OrderCard";
 import "./style.css";
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext);
+  console.log("CART:", context.cartProducts);
 
   return (
     <aside
@@ -20,6 +22,16 @@ const CheckoutSideMenu = () => {
             onClick={() => context.closeCheckoutSideMenu()}
           ></XMarkIcon>
         </div>
+      </div>
+      <div className="px-6">
+        {context.cartProducts.map((product) => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+          />
+        ))}
       </div>
     </aside>
   );
---
+++ src/Components/OrderCard/index.jsx
@@ -0,0 +1,26 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
+const OrderCard = (props) => {
+  const { title, imageUrl, price } = props;
+
+  return (
+    <div className="flex justify-between items-center mb-4 ">
+      <div className="flex items-center gap-2">
+        <figure className="w-12 h-12">
+          <img
+            className="w-full h-full rounded-lg object-cover"
+            src={imageUrl}
+            alt={title}
+          />
+        </figure>
+        <p className="text-sm font-light">{title}</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <p className="text-lg font-medium">{price}</p>
+        <XMarkIcon className="w-6 h-6 cursor-pointer"></XMarkIcon>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCard;
